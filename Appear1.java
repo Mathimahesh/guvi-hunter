ef871326856e9fd8c1110f953d92cad99a847766
MaheshwariS <36835393+Mathimahesh@users.noreply.github.com>
2019-06-10 12:12:18 +0530

    Create Appear1.java

---
+++ Appear1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for(int i=0;i<n;i++){
+            a[i] = sc.nextInt();
+        }
+        Arrays.sort(a);
+        int val = 0;
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a.length;j++){
+                if(a[i]!=a[j]){
+                    val = a[i];
+                    break;
+                }
+            }
+        }
+        System.out.println(val);
+        
+    }
+}
